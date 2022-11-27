0663ea0a0ce8e87b2efe3d8cda70fd266f854d80
Hopd84 <Ca_dict@mail.ru>
2022-11-27 17:51:06 +0300

    Enable caching in ApplicationRunner

--- src/main/java/spb/ifuture/ru/ApplicationRunner.java
+++ src/main/java/spb/ifuture/ru/ApplicationRunner.java
@@ -2,12 +2,13 @@ package spb.ifuture.ru;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableCaching
 public class ApplicationRunner {
-
     public static void main(String[] args) {
         SpringApplication.run(ApplicationRunner.class, args);
     }
