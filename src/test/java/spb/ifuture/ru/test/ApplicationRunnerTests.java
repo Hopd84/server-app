7b7d9fb4d570ca094221afe897afaff7ff5dad5f
Hopd84 <Ca_dict@mail.ru>
2022-11-27 15:16:28 +0300

    Add ApplicationRunnerTests

---
+++ src/test/java/spb/ifuture/ru/test/ApplicationRunnerTests.java
@@ -0,0 +1,13 @@
+package spb.ifuture.ru.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationRunnerTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
