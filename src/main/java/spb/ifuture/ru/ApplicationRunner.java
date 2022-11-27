5ea227e258e0526cb07aaf643818640c7444a500
Hopd84 <Ca_dict@mail.ru>
2022-11-27 15:10:12 +0300

    Add ApplicationRunner

---
+++ src/main/java/spb/ifuture/ru/ApplicationRunner.java
@@ -0,0 +1,14 @@
+package spb.ifuture.ru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class ApplicationRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationRunner.class, args);
+    }
+}
